fd95316d6960e68656efb6c13230c8899970b071
CHGary823 <chchinghong@gmail.com>
2024-08-07 23:47:14 +0800

    Interface Update

---
+++ src/HashInterface.java
@@ -0,0 +1,10 @@
+public interface Hash<K,V>{
+        boolean insert(K key, V value); // Insert a key-value pair
+        boolean remove(K key);
+        void clear(); // Clear all entries
+        boolean replace(K key, V oldValue, V newValue); // Replace old value with new value for a given key
+        boolean containsKey(K key);
+        boolean containsValue(V value); // Check if the map contains the specified value
+        V get(K key); // Get the value associated with the specified key
+    }
+
---
+++ src/TreeInterface.java
@@ -0,0 +1,8 @@
+public interface Tree<K> {
+        boolean insert(K key);
+        boolean remove(K key); // Remove the entry with the specified key
+        void clear(); // Clear all entries
+        boolean containsKey(K key); // Check if the map contains the specified key
+        boolean isEmpty(); // Check if the map is empty
+
+}
